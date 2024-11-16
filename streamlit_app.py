1ece6c10f33dc4e02775e364c75511ca8ec0275e
jamshidsalimov <jamshidsalimov8@gmail.com>
2024-11-16 13:51:30 +0500

    trhrt
    rth

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,8 @@ from fastai.vision.all import *
 
 import pathlib
 import plotly.express as px
-import platform
-plt=platform.system()
-if plt == 'Linux':pathlib.WindowsPath = pathlib.PosixPath
-
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 # title
 st.title('Transportni klassifikatsiya qiluvchi model')
 
