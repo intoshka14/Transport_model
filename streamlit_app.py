606b6ee368cad2e70c07a96a060fb0c6be87d1f6
jamshidsalimov <jamshidsalimov8@gmail.com>
2024-11-16 14:11:50 +0500

    wqd
    qwd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,57 @@
+# import streamlit as st
+# from fastai.vision.all import *
+
+# import pathlib
+# import plotly.express as px
+# temp = pathlib.PosixPath
+# pathlib.PosixPath = pathlib.WindowsPath
+# # title
+# st.title('Transportni klassifikatsiya qiluvchi model')
+
+# # rasmni joylash
+# files=st.file_uploader("Rasm yuklash", type=["avif","png","jpeg","gif","svg"])
+# if files:
+#     st.image(files) # rasmni chiqarish
+#     # PIL convert
+#     img=PILImage.create(files)
+#     model = load_learner('transport_model.pkl')
+    
+#     # Bashorat qiymatni topamiz
+#     pred,pred_id, probs =model.predict(img)
+#     st.success(f"Bashorat: {pred}")
+#     st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+
+#     # plotting
+#     fig=px.bar(x=probs*100, y=model.dls.vocab)
+#     st.plotly_chart(fig)
+
+
+
+
 import streamlit as st
 from fastai.vision.all import *
-
-import pathlib
 import plotly.express as px
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+import pathlib
+pathlib.PosixPath = pathlib.Path
+
 # title
 st.title('Transportni klassifikatsiya qiluvchi model')
 
-# rasmni joylash
-files=st.file_uploader("Rasm yuklash", type=["avif","png","jpeg","gif","svg"])
+# Rasmni joylash
+files = st.file_uploader("Rasm yuklash", type=["avif", "png", "jpeg", "gif", "svg"])
 if files:
-    st.image(files) # rasmni chiqarish
+    st.image(files)  # rasmni chiqarish
     # PIL convert
-    img=PILImage.create(files)
-    model = load_learner('transport_model.pkl')
+    img = PILImage.create(files)
     
-    # Bashorat qiymatni topamiz
-    pred,pred_id, probs =model.predict(img)
-    st.success(f"Bashorat: {pred}")
-    st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+    # Modelni yuklash
+    model = load_learner('transport_model.pkl')
 
-    # plotting
-    fig=px.bar(x=probs*100, y=model.dls.vocab)
+    # Bashorat qiymatni topamiz
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Ehtimollik: {probs[pred_id] * 100:.1f}%")
+
+    # Plotting
+    fig = px.bar(x=probs * 100, y=model.dls.vocab)
     st.plotly_chart(fig)
